9a0c24b795ee5ed46a7e51aac59f421ab2b61324
AbominablePutridity <qq594492@gmail.com>
2026-01-03 16:56:22 +0300

    Разрешение CORS для всех сторонних сайтов (для взятия данных Frontend-приложению из api)

--- src/main/java/com/mycompany/cardapiservice/configuration/SecurityConfig.java
+++ src/main/java/com/mycompany/cardapiservice/configuration/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.mycompany.cardapiservice.configuration;
 
 import com.mycompany.cardapiservice.enums.Role;
 import com.mycompany.cardapiservice.security.JwtAuthenticationFilter;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 /**
  * Этот сервис - главное сердце безоапсности
@@ -41,6 +44,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                 // Отключаем CORS (для фронта)
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOriginPatterns(List.of("*"));
+                    config.setAllowedMethods(List.of("*"));
+                    config.setAllowedHeaders(List.of("*"));
+                    config.setAllowCredentials(true);
+                    return config;
+                }))
+                
                 .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/api/users/guest/getEntryTocken",   // роут для входа пользователя (взятие токена)
                             "/public/**", "/static/**",
