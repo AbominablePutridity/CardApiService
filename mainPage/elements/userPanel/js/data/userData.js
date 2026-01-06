3f9def9e548f34fd132edad8c83975362ba337e1
AbominablePutridity <qq594492@gmail.com>
2026-01-06 20:18:02 +0300

    Завершение создания входа для клиентов

--- mainPage/elements/userPanel/js/data/userData.js
+++ mainPage/elements/userPanel/js/data/userData.js
@@ -1,9 +1,34 @@
-const userData = {
-  "id": 1,
-  "isBlocked": null,
-  "login": "ivanov",
-  "name": "Иван",
-  "patronymic": "Иванович",
-  "role": "USER",
-  "surname": "Иванов"
-};
+let userData = null; // Здесь будут наши данные карт текущего пользователя
+
+//берем данные о текущем пользователе из API
+dataGet(
+    sessionStorage.getItem('jwtToken'), // берем наш токен из сессии
+    `api/users/user/getCurrentUserObject`,
+    
+  ).done(function(data) {
+    // ДАННЫЕ ЗДЕСЬ!
+    console.log("The data = ", data);
+
+    userData = data; //приравниваем данные из api в массив cardsData, из которых будем брать данные о картах пользователя на отрисовку
+
+    // Вызов функции для вставки данных из массива на страницу
+    // (т.к. блок .done - область видимости полученых данных из запроса)
+    setUserData();
+  }).fail(function() {
+    alert("Login failed!");
+
+    //если не смогли получить данные о текущем пользователе из API,
+    // то это значит, что пользователь не вошел и не получил свой токен - перенаправляем на страницу входа!
+    window.location.href = "../loginPage/login.html";
+});
+
+//const userData =
+// {
+//   "id": 1,
+//   "isBlocked": null,
+//   "login": "ivanov",
+//   "name": "Иван",
+//   "patronymic": "Иванович",
+//   "role": "USER",
+//   "surname": "Иванов"
+// };
--- mainPage/elements/userPanel/js/userPanelDataManager.js
+++ mainPage/elements/userPanel/js/userPanelDataManager.js
@@ -1,4 +1,12 @@
-userPanel = document.getElementById("username");
+// скрипт, для отрисовки получченных данных из запроса о текущем пользователе на главную страницу
 
-userPanel.textContent = userData.surname + " " + userData.name + " " + userData.patronymic;
+/**
+ * Отрисоввывает ФИО пользователя на главной
+ */
+function setUserData()
+{
+  userPanel = document.getElementById("username");
+
+  userPanel.textContent = userData.surname + " " + userData.name + " " + userData.patronymic;
+}
 
