4d778d50218a4a7bdecb6a4e290c3a5ab6bce1d0
AbominablePutridity <qq594492@gmail.com>
2026-01-09 18:53:40 +0300

    Начало создания вкладки с транзакциями карты: получение данных из АПИ. Доработка функционала вкладки - О карте

--- mainPage/elements/card/js/cardDataManager.js
+++ mainPage/elements/card/js/cardDataManager.js
@@ -20,7 +20,8 @@ function renderCards(page) {
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = Math.min(startIndex + itemsPerPage, cardsData.length);
 
-    // Используем классический цикл for для пагинации
+    // Используем классический цикл for для пагинации 
+    //(обходим каждую карту в цикле для установки данных)
     for (let i = startIndex; i < endIndex; i++) {
         const data = cardsData[i]; // Берем текущий элемент массива по индексу
 
@@ -48,13 +49,13 @@ function renderCards(page) {
             setCurrentCardId(buttonInfo.id);
             setTabForCardData(data); // тут data - это один конкретный обьект из за цикла!!!
         });
-
+        
         // Добавляем готовую карточку в список
         cardList.appendChild(cardElement);
     }
 }
 
-//Пагинация карт
+//Пагинация карт - вне функции
 btnBack = document.getElementById("pag-card-button-back");
 btnForward = document.getElementById("pag-card-button-forward");
 
@@ -72,4 +73,18 @@ btnForward.addEventListener('click', function(event) {
 
             renderCards(currentPage);
         }
+});
+
+// обработчик кнопок управления вкладками вне функции - для того чтобы не вешался несколько раз, каждый раз когда вызывается функция
+let buttonCardInfo = document.getElementById("tab1");
+buttonCardInfo.addEventListener('click', function() {
+    if(getCurrentCardId() != null) { // если карта выбранна (в скрипте tabManager есть id выбранной карты)
+        setTabForCardData(cardsData.find(item => item.id == getCurrentCardId())); //то из массива карт выбираем тот элемент, который соответствует Id из скрипта tabManager
+    }
+});
+
+// обработчик кнопок управления вкладками вне функции - для того чтобы не вешался несколько раз, каждый раз когда вызывается функция
+let buttonTranzaction = document.getElementById("tab2");
+buttonTranzaction.addEventListener('click', function() {
+   setTabForCardTranzactions(getCurrentCardId(), 0, 10); // тут data - это один конкретный обьект из за цикла!!!
 });
--- mainPage/elements/tabs/tabManager.js
+++ mainPage/elements/tabs/tabManager.js
@@ -5,6 +5,13 @@ let div = document.getElementById('screens');
 let currentCardId = null; //для взятия данных о конкретной карте по ее Id-коду
 
 /**
+ * для взятия значения Id карты.
+ */
+function getCurrentCardId() {
+    return currentCardId;
+}
+
+/**
  * Для установки какого либо действия над данными конкретной карты, задаем ее Id - для взятия по нему данных.
  * @param cardId Id-код карты.
  */
@@ -12,6 +19,9 @@ function setCurrentCardId(cardId) {
     console.log("CurrentCardId = ", cardId);
     currentCardId = cardId;
 }
+
+// переменная, хранящая массив с данными как результат выполнения запроса
+let dataForRendering = null;
 
 /**
  * Отрисоввывает информацию о каждой карте на вкладке "О карте" по данным карты
@@ -33,4 +43,28 @@ function setTabForCardData(data)
       <button>Совершить перевод</button>
     </p>
     `;
+}
+
+/**
+ * Функция со сбором данных о транзакциях, и в дальнейшем, ее отрисовки.
+ * @param cardId Id карты, у которой берем все транзакции.
+ * @param page Страница (для пагинации).
+ * @param size Количество элементов на странице (для пагинации).
+ */
+function setTabForCardTranzactions(cardId, page, size)
+{
+    //берем данные о текущем пользователе из API
+    dataGet(
+        sessionStorage.getItem('jwtToken'), // берем наш токен из сессии
+        `api/cardTransfer/user/getAllTransfersToUserCard?page=${page}&size=${size}&idUserCard=${cardId}`,
+        
+    ).done(function(data) {
+        // ДАННЫЕ ЗДЕСЬ!
+        console.log("All tranzactions = ", data);
+
+        dataForRendering = data; //приравниваем данные из api в массив dataForRendering, из которых будем брать данные о транзакциях
+
+    }).fail(function() {
+        alert("Shown transactions failed!");
+    });
 }
