9935e8d316f7dcee9121e2f70f028c6431cfc71e
AbominablePutridity <qq594492@gmail.com>
2026-01-10 21:08:44 +0300

    Создание пагинации для вкладки с транзакциями

--- mainPage/elements/tabs/tabManager.js
+++ mainPage/elements/tabs/tabManager.js
@@ -1,5 +1,10 @@
 // Скрипт, отрисоввывающий на вкладках информацию о картах
 
+let currentTranzCardId = null;
+let currentTranzPage = 0;
+let currentTranzSize = 10;
+let currentTranzTotalPages = 0;
+
 // Находим элемент
 let div = document.getElementById('screens');
 let currentCardId = null; //для взятия данных о конкретной карте по ее Id-коду
@@ -45,48 +50,67 @@ function setTabForCardData(data)
     `;
 }
 
+ 
 /**
  * Функция со сбором данных о транзакциях, и в дальнейшем, ее отрисовки.
  * @param cardId Id карты, у которой берем все транзакции.
  * @param page Страница (для пагинации).
  * @param size Количество элементов на странице (для пагинации).
  */
-function setTabForCardTranzactions(cardId, page, size)
-{
-    //берем данные о текущем пользователе из API
+function setTabForCardTranzactions(cardId, page, size) {
+    console.log(`Запрос: cardId=${cardId}, page=${page}, size=${size}`);
+    
     dataGet(
-        sessionStorage.getItem('jwtToken'), // берем наш токен из сессии
+        sessionStorage.getItem('jwtToken'),
         `api/cardTransfer/user/getAllTransfersToUserCard?page=${page}&size=${size}&idUserCard=${cardId}`,
+    ).done(function(data) {
+        console.log("Получены данные:", data);
         
-    ).done(function(data) { // блок с успешным получением данных
-        // ДАННЫЕ ЗДЕСЬ!
-        console.log("All tranzactions = ", data);
-
-        dataForRendering = data; //приравниваем данные из api в массив dataForRendering, из которых будем брать данные о транзакциях
-
+        // ПРОСТО отрисовываем что получили
         div.innerHTML = 
         `
         <div class="items-container">
-        ${dataForRendering.map(item => `
+        ${data.map(item => `
             <div class="item">
             <h3 class="main-title">${item.description}</h3>
             <p class="info-text">
-                <h1> ${item.amountOfMoney} </h1>
+                <h1>${item.amountOfMoney} ${item.receiverDto?.currencyDto?.sign || ''}</h1>
             </p>
-
             <p class="info-details">
-                Счет получателя: ${item.receiverDto.number} </br>
-                Отправитель: ${item.senderDto.userDto.surname} ${item.senderDto.userDto.name} ${item.senderDto.userDto.patronymic}</br>
-                Получатель: ${item.receiverDto.userDto.surname} ${item.receiverDto.userDto.name} ${item.receiverDto.userDto.patronymic}</br>
+                Счет получателя: ${item.receiverDto?.number || 'Нет данных'} </br>
+                Отправитель: ${item.senderDto?.userDto?.surname || ''} ${item.senderDto?.userDto?.name || ''} ${item.senderDto?.userDto?.patronymic || ''}</br>
+                Получатель: ${item.receiverDto?.userDto?.surname || ''} ${item.receiverDto?.userDto?.name || ''} ${item.receiverDto?.userDto?.patronymic || ''}</br>
             </p>
-            
             <span class="date">${item.transferDate}</span>
             </div>
             <hr>
         `).join('')}
         </div>
+        
+        <div class="pagination-container default-text default-panel">
+            <button id="pag-tranz-button-back">← Назад</button>
+            <span id="page-info"> Страница ${page + 1} </span>
+            <button id="pag-tranz-button-forward">Вперед →</button>
+        </div>
         `;
+        
+        // ВЕШАЕМ ОБРАБОТЧИКИ ПРЯМО СЕЙЧАС
+        const btnBack = document.getElementById("pag-tranz-button-back");
+        const btnForward = document.getElementById("pag-tranz-button-forward");
+        
+        // КНОПКА "НАЗАД" - запрос с page-1
+        btnBack.onclick = function() {
+            console.log("Нажата кнопка Назад!");
+            setTabForCardTranzactions(cardId, page - 1, size);
+        };
+        
+        // КНОПКА "ВПЕРЕД" - запрос с page+1
+        btnForward.onclick = function() {
+            console.log("Нажата кнопка Вперед!");
+            setTabForCardTranzactions(cardId, page + 1, size);
+        };
+        
     }).fail(function() {
-        alert("Для получения транзакций карты сначала необходимо выбрать карту!");
+        alert("Ошибка загрузки транзакций!");
     });
 }
